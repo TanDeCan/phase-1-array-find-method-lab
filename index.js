83f4247677127564b30bd9c4ecb6e89381e4b6cb
VictoriaBladow <vbladow9999@yahoo.com>
2022-11-13 14:44:29 -0600

    commit tree 4df425837b6f71fd396040f035d0d40f72a9062f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array){
+    for (const object of array)
+    if (object.result === "W"){
+        return object.year
+    }
+}
+
+record.find(superbowlWin)
